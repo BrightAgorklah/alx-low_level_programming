cb96308d832cd8db8d226a9a4f897430b48a4217
BrightAgorklah <agorklah.k.bright@gmail.com>
2023-02-19 00:00:59 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+/**
+*main - A programme that print a line with put function
+*Return: 0 (sucess)
+*/
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle"");
+return(0);
+}
