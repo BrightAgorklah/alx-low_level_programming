5153f4783173de0e87369ec738fbb59df83cc1e0
BrightAgorklah <agorklah.k.bright@gmail.com>
2023-02-27 23:55:23 -0800

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* mul - multiplies two integers
+* @a: first integer
+* @b: second integer
+* owned by /BrightAgorklah
+* Return: addition of a and b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
